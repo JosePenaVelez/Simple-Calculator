2d12874af5b073bc378c2a86cfd4c32ff7768a06
Jose Joel Pena Velez <115565113+JosePenaVelez@users.noreply.github.com>
2024-07-30 20:55:56 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple-Calculator
+
+# About:
+    A simple calculator that can multiply, divide, add or subtract two numbers.# Simple-Calculator
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+double calculate (double a, double b, char op){
+    switch (op)
+    {
+    case '*': return a*b;
+    case '/': {
+        if(b==0){
+            std::cout<<"Error: Not divisible";
+            return 0;
+        }
+        else
+            return a/b;
+        }
+    case '+': return a+b;
+    case '-': return a-b;
+    
+    default:
+        std::cout<<"Error: Enter the correct operator";
+        return 0;
+    }
+}
+
+int main (){
+    double a,b,res;
+    char op;
+    std::cout<<"Enter 1st Number:";
+    std::cin>>a;
+    std::cout<<"Enter Operator (*/+-):";
+    std::cin>>op;
+    std::cout<<"Enter 2nd Number:";
+    std::cin>>b;
+
+    res = calculate(a,b,op);
+    std::cout<<"\nResult: " << res;
+    return 0;
+}
